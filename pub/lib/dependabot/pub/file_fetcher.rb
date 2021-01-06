6f8e6ce4e1063f85dfe5e9a566a2e3d9af802b0a
Johann Schramm <43448334+JohannSchramm@users.noreply.github.com>
2021-01-06 20:52:12 +0100

    Add pub file fetcher

--- pub/lib/dependabot/pub/file_fetcher.rb
+++ pub/lib/dependabot/pub/file_fetcher.rb
@@ -6,6 +6,30 @@ require "dependabot/file_fetchers/base"
 module Dependabot
   module Pub
     class FileFetcher < Dependabot::FileFetchers::Base
+      def self.required_files_in?(filenames)
+        filenames.include?("pubspec.yaml")
+      end
+
+      def self.required_files_message
+        "Directory must contain a pubspec.yaml"
+      end
+
+      private
+
+      def fetch_files
+        files = []
+
+        files << pubspec if pubspec
+
+        return files if files.any?
+
+        path = Pathname.new(File.join(directory, "pubspec.yaml")).cleanpath.to_path
+        raise Dependabot::DependencyFileNotFound, path
+      end
+
+      def pubspec
+        @pubspec ||= fetch_file_if_present("pubspec.yaml")
+      end
     end
   end
 end
--- pub/spec/dependabot/pub/file_fetcher_spec.rb
+++ pub/spec/dependabot/pub/file_fetcher_spec.rb
@@ -5,4 +5,72 @@ require "dependabot/pub/file_fetcher"
 require_common_spec "file_fetchers/shared_examples_for_file_fetchers"
 
 RSpec.describe Dependabot::Pub::FileFetcher do
+  it_behaves_like "a dependency file fetcher"
+
+  let(:source) do
+    Dependabot::Source.new(
+      provider: "github",
+      repo: "JohannSchramm/dependabot-pub-example",
+      directory: "/"
+    )
+  end
+  let(:credentials) do
+    [{
+      "type" => "git_source",
+      "host" => "github.com",
+      "username" => "x-access-token",
+      "password" => "token"
+    }]
+  end
+  let(:file_fetcher_instance) do
+    described_class.new(source: source, credentials: credentials)
+  end
+
+  let(:url) { "https://api.github.com/repos/JohannSchramm/dependabot-pub-example/contents/" }
+
+  let(:json_header) { { "content-type" => "application/json" } }
+  before { allow(file_fetcher_instance).to receive(:commit).and_return("sha") }
+
+  context "with a pubspec.yaml file" do
+    before do
+      stub_request(:get, url + "?ref=sha").
+        with(headers: { "Authorization" => "token token" }).
+        to_return(
+          status: 200,
+          body: fixture("github", "contents_repo.json"),
+          headers: json_header
+        )
+      stub_request(:get, url + "pubspec.yaml?ref=sha").
+        with(headers: { "Authorization" => "token token" }).
+        to_return(
+          status: 200,
+          body: fixture("github", "contents_pubspec.json"),
+          headers: json_header
+        )
+    end
+
+    it "fetches the pubspec.yaml file" do
+      expect(file_fetcher_instance.files.map(&:name)).to match_array(
+        %w(pubspec.yaml)
+      )
+    end
+  end
+
+  context "without a pubspec.yaml file" do
+    before do
+      stub_request(:get, url + "?ref=sha").
+        with(headers: { "Authorization" => "token token" }).
+        to_return(
+          status: 200,
+          body: fixture("github", "contents_repo_without_pubspec.json"),
+          headers: json_header
+        )
+    end
+
+    it "raises a helpful error" do
+      expect { file_fetcher_instance.files }.to raise_error(
+        Dependabot::DependencyFileNotFound
+      )
+    end
+  end
 end
