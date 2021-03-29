cc1f052bab7df3c45b0339d0d7856ae715bf2515
Johann Schramm <43448334+JohannSchramm@users.noreply.github.com>
2021-03-30 01:43:45 +0200

    Add pub metadata finder

--- pub/lib/dependabot/pub/metadata_finder.rb
+++ pub/lib/dependabot/pub/metadata_finder.rb
@@ -6,6 +6,26 @@ require "dependabot/metadata_finders/base"
 module Dependabot
   module Pub
     class MetadataFinder < Dependabot::MetadataFinders::Base
+      private
+
+      def look_up_source
+        pubspec = package_info["latest"]["pubspec"]
+        repo = pubspec["homepage"] unless pubspec["homepage"].nil?
+        repo = pubspec["repository"] unless pubspec["repository"].nil?
+        Source.from_url(repo)
+      end
+
+      def package_info
+        res = Excon.get(
+          "https://pub.dartlang.org/api/packages/#{dependency.name}",
+          idempotent: true,
+          **Dependabot::SharedHelpers.excon_defaults
+        )
+
+        raise "Unable to get package info" unless res.status == 200
+
+        JSON.parse(res.body)
+      end
     end
   end
 end
