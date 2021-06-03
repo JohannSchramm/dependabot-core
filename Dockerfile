e840bb72c0d89a44e23c84bef26326a90dded51f
Johann Schramm <43448334+JohannSchramm@users.noreply.github.com>
2021-06-03 15:42:25 +0200

    Add pubspec.lock file updater

--- Dockerfile
+++ Dockerfile
@@ -198,6 +198,17 @@ RUN wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb \
   && rm -rf /var/lib/apt/lists/*
 
 
+### DART
+
+# Install Dart and Pub
+RUN apt-get install apt-transport-https \
+  && sh -c 'wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -' \
+  && sh -c 'wget -qO- https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list' \
+  && apt-get update \
+  && apt-get install dart
+ENV PATH="$PATH:/usr/lib/dart/bin"
+
+
 ### RUST
 
 # Install Rust 1.51.0
--- pub/spec/dependabot/pub/file_updater_spec.rb
+++ pub/spec/dependabot/pub/file_updater_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require "dependabot/shared_helpers"
 require "dependabot/pub/file_updater"
 require_common_spec "file_updaters/shared_examples_for_file_updaters"
 
@@ -23,7 +24,68 @@ RSpec.describe Dependabot::Pub::FileUpdater do
     )
   end
 
-  context "for a dependency file" do
+  context "for a pubspec.yaml and a pubspec.lock file" do
+    subject(:updated_files) { file_updater_instance.updated_dependency_files }
+
+    context "with a dependency" do
+      let(:dependencies) do
+        [
+          Dependabot::Dependency.new(
+            name: "a",
+            version: nil,
+            package_manager: "pub",
+            requirements: [{
+              requirement: "^2.1.0",
+              groups: ["dependencies"],
+              source: nil,
+              file: "pubspec.yaml"
+            }],
+            previous_version: nil,
+            previous_requirements: [{
+              requirement: "^1.0.0",
+              groups: ["dependencies"],
+              source: nil,
+              file: "pubspec.yaml"
+            }]
+          )
+        ]
+      end
+
+      let(:dependency_files) do
+        [
+          Dependabot::DependencyFile.new(
+            name: "pubspec.yaml",
+            content: fixture("pubspec", "outdated_pubspec.yaml")
+          ),
+          Dependabot::DependencyFile.new(
+            name: "pubspec.lock",
+            content: "test pubspec.lock content"
+          )
+        ]
+      end
+
+      it "should update the dependency and lockfile" do
+        expect(Dependabot::SharedHelpers).to receive(:run_shell_command).with("dart pub upgrade a")
+
+        allow(File).to receive(:read).and_call_original
+        allow(File).to receive(:read).with("pubspec.lock").and_return("updated pubspec.lock content")
+
+        expected_files = [
+          Dependabot::DependencyFile.new(
+            name: "pubspec.yaml",
+            content: fixture("pubspec", "updated_pubspec.yaml")
+          ),
+          Dependabot::DependencyFile.new(
+            name: "pubspec.lock",
+            content: "updated pubspec.lock content"
+          )
+        ]
+        expect(updated_files).to eq(expected_files)
+      end
+    end
+  end
+
+  context "for a pubspec.yaml file" do
     subject(:updated_files) { file_updater_instance.updated_dependency_files }
 
     context "with a dependency" do
